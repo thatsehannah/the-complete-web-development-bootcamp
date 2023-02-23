566b6648ad439395a93f15522d9c66f96cf0f042
E. Hannah <echannah631@gmail.com>
2023-02-22 18:29:19 -0600

    winner logic

--- dice-game/index.js
+++ dice-game/index.js
@@ -5,3 +5,11 @@ document.querySelector(".img1").setAttribute("src", img1);
 var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
 var img2 = "images/dice"+randomNumber2+".png";
 document.querySelector(".img2").setAttribute("src", img2);
+
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩"
+} else {
+  document.querySelector("h1").innerHTML = "Draw!"
+}
