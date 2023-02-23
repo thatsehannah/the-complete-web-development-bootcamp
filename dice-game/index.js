c3f73ae676ebe77a0b40d4824e54b7477055ec00
E. Hannah <echannah631@gmail.com>
2023-02-22 18:25:28 -0600

    generating random di images for both

--- dice-game/index.js
+++ dice-game/index.js
@@ -1,1 +1,7 @@
 var randomNumber1 = (Math.floor(Math.random() * 6) + 1);
+var img1 = "images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src", img1);
+
+var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
+var img2 = "images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src", img2);
