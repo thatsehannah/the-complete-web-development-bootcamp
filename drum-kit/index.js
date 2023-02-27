1afd5fd90db4c58bdea8e916a91eb789f7c47bc8
E. Hannah <echannah631@gmail.com>
2023-02-27 08:34:50 -0600

    detecting keyboard events and button clicks

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,3 +1,41 @@
-document.querySelectorAll(".drum").forEach((e) => e.addEventListener("click", () => {
-  //new Audio('sounds/tom-1.mp3').play();
-}));
+var drums = document.querySelectorAll('.drum');
+
+// Detecting button click on the individual buttons
+for (var i = 0; i < drums.length; i++) {
+  drums[i].addEventListener('click', function () {
+    makeSound(this.innerHTML);
+  });
+}
+
+// Detecting keyboard event (adding to the entire page)
+document.addEventListener('keydown', function (e) {
+  makeSound(e.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case 'w':
+      new Audio('sounds/crash.mp3').play();
+      break;
+    case 'a':
+      new Audio('sounds/kick-bass.mp3').play();
+      break;
+    case 's':
+      new Audio('sounds/snare.mp3').play();
+      break;
+    case 'd':
+      new Audio('sounds/tom-1.mp3').play();
+      break;
+    case 'j':
+      new Audio('sounds/tom-2.mp3').play();
+      break;
+    case 'k':
+      new Audio('sounds/tom-3.mp3').play();
+      break;
+    case 'l':
+      new Audio('sounds/tom-4.mp3').play();
+      break;
+    default:
+      console.log(this.innerHTML);
+  }
+}
