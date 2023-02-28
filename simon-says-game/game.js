ff3cb22a996a8e87c14b436c94c418dfda695d70
E. Hannah <echannah631@gmail.com>
2023-02-27 21:25:56 -0600

    smol refactor

--- simon-says-game/game.js
+++ simon-says-game/game.js
@@ -2,13 +2,13 @@ var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 var userClickedPattern = [];
 var level = 0;
-var gameIsGoing = true;
 
 function playSound(name) {
   new Audio('sounds/' + name + '.mp3').play();
 }
 
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $('#level-title').text('Level ' + level);
   var randomNum = Math.floor(Math.random() * 3);
@@ -37,19 +37,18 @@ function checkAnswer(currentLevel) {
   console.log(userClickedPattern);
   if (userClickedPattern[currentLevel] !== gamePattern[currentLevel]) {
     playSound('wrong');
+    $('#level-title').text('Game over. Press any key to play again.');
     $("body").addClass("game-over");
     setTimeout(function () {
       $('body').removeClass("game-over");
     }, 200);
+
     gamePattern = [];
-    userClickedPattern = [];
-    $('#level-title').text('Game over. Press any key to play again.');
     level = 0;
   } else {
     if (userClickedPattern.length == gamePattern.length) { // if check is to see if the user is done with recreating the sequence correctly
       setTimeout(function () {
         nextSequence();
-        userClickedPattern = [];
       }, 1000);
     }
   }
