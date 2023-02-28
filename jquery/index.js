f787de690f13093a923d294eddb27c13f22d97d1
E. Hannah <echannah631@gmail.com>
2023-02-27 19:36:12 -0600

    changing attributes & adding event listeners using jquery

--- jquery/index.js
+++ jquery/index.js
@@ -1,3 +1,13 @@
-$('h1').addClass("big-title margin-50")
-$('h1').text("What's up?");
-$('button').html("<em>Hey</em>");
+$('a').attr('href', 'https://yahoo.com');
+$('button').click(function (e) {
+  e.preventDefault();
+  $('h1').css('color', 'purple');
+});
+
+$(document).keydown(function (e) {
+  $('h1').text(e.key);
+});
+
+$('h1').on('mouseover', function () {
+  $('h1').css('color', 'purple');
+});
