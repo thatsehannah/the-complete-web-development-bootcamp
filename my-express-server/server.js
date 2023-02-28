5ec22c4e5a4fa60521510b2ea32ad058a7ed154f
E. Hannah <echannah631@gmail.com>
2023-02-28 13:08:46 -0600

    testing out nodemon

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -7,12 +7,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
-  res.send('Contact me at: ehannah@gmail.com')
-})
+  res.send('Contact me at: ehannah@gmail.com');
+});
 
 app.get('/about', (req, res) => {
-  res.send("My name is Elliot Hannah and I am a Full Stack Developer from Houston, Texas by way of the streets of Augusta, Georgia.")
-})
+  res.send(
+    'My name is Elliot Hannah and I am a Full Stack Developer from Houston, Texas by way of the streets of Augusta, Georgia.'
+  );
+});
+
+app.get('/hobbies', (req, res) => {
+  res.send('<ul><li>2K</li><li>Music</li><li>Coding</li></ul>');
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
