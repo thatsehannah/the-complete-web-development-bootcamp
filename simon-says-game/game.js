4096cdccc6aef6d7297a54158a04766d2be01faa
E. Hannah <echannah631@gmail.com>
2023-02-27 20:23:10 -0600

    adding in sounds, flash and storing the clicked colors

--- simon-says-game/game.js
+++ simon-says-game/game.js
@@ -1,10 +1,22 @@
-var buttonColors = ["red", "blue", "green", "yellow"]
+var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
   var randomNum = Math.floor(Math.random() * 3);
   var randomChosenColor = buttonColors[randomNum];
   gamePattern.push(randomChosenColor);
+
+  $('#' + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+    new Audio("sounds/" + randomChosenColor + ".mpe").play();
 }
 
-
+$(".btn").click(function (e) { 
+  var userChosenColor = e.target.id;
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
+});
