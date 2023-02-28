dc8d7fa8fde236c3f78aa781d82a4ffb285c21bd
E. Hannah <echannah631@gmail.com>
2023-02-27 21:22:14 -0600

    added logic to check user's sequence against game's sequence

--- simon-says-game/game.js
+++ simon-says-game/game.js
@@ -1,8 +1,16 @@
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var gameIsGoing = true;
+
+function playSound(name) {
+  new Audio('sounds/' + name + '.mp3').play();
+}
 
 function nextSequence() {
+  level++;
+  $('#level-title').text('Level ' + level);
   var randomNum = Math.floor(Math.random() * 3);
   var randomChosenColor = buttonColors[randomNum];
   gamePattern.push(randomChosenColor);
@@ -12,15 +20,51 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-    playSound(randomChosenColor);
+  playSound(randomChosenColor);
 }
 
-$(".btn").click(function (e) { 
+function animatePress(currentColor) {
+  $('#' + currentColor).addClass('pressed');
+
+  setTimeout(function () {
+    $('#' + currentColor).removeClass('pressed');
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  console.log(currentLevel);
+  console.log(gamePattern);
+  console.log(userClickedPattern);
+  if (userClickedPattern[currentLevel] !== gamePattern[currentLevel]) {
+    playSound('wrong');
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $('body').removeClass("game-over");
+    }, 200);
+    gamePattern = [];
+    userClickedPattern = [];
+    $('#level-title').text('Game over. Press any key to play again.');
+    level = 0;
+  } else {
+    if (userClickedPattern.length == gamePattern.length) { // if check is to see if the user is done with recreating the sequence correctly
+      setTimeout(function () {
+        nextSequence();
+        userClickedPattern = [];
+      }, 1000);
+    }
+  }
+}
+
+$('.btn').click(function (e) {
   var userChosenColor = e.target.id;
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
-function playSound(name) {
-  new Audio("sounds/" + name + ".mp3").play();
-}
+$(document).keydown(function (e) {
+  if (level == 0) {
+    nextSequence();
+  }
+});
