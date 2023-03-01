0caa7de988de82cad564d22a282839c9ab6ae1fa
E. Hannah <echannah631@gmail.com>
2023-03-01 10:10:26 -0600

    parsing & processing data that's sent from the from

--- my-express-server/calculator/calculator.js
+++ my-express-server/calculator/calculator.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(__dirname + "/index.html");
+  // console.log(__dirname + "/index.html");
 });
+
+app.post('/', (req, res) => {
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+
+  res.send("The result of the calculation is " + (num1 + num2));
+})
 
 app.listen(3000, () => {
   console.log('App is listening on port 3000.');
