8047779d083658a9624584d7a209c71591b68db0
E. Hannah <echannah631@gmail.com>
2023-03-02 10:10:17 -0600

    using bodyparser to get user input for specific city

--- weather-project/app.js
+++ weather-project/app.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const https = require('https');
-
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
 const apiKey = process.env.API_KEY;
 
 app.get('/', (req, res) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=${apiKey}&units=imperial`;
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  const query = req.body.cityName
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=imperial`;
 
   https.get(url, (response) => {
     response.on("data", (data) => {
@@ -17,12 +24,14 @@ app.get('/', (req, res) => {
       const icon = weatherData.weather[0].icon
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
       res.write(`<p>The weather is currently ${description}.</p>`)
-      res.write(`<h1>The temperature in Houston is ${temp} degrees Fahrenheit.</h1>`)
+      res.write(`<h1>The temperature in ${query} is ${temp} degrees Fahrenheit.</h1>`)
       res.write(`<img src=${imageURL} alt='weather-icon'>`)
       res.send()
     })
   })
-});
+})
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
