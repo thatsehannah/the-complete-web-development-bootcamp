7b82b6d4abc18d1d4acb88f7c66adcb2b192c468
E. Hannah <echannah631@gmail.com>
2023-03-02 13:35:38 -0600

    created post request to mailchimp api

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 app.use(express.urlencoded({ extended: true })); // replaces body-parser
@@ -10,12 +12,44 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-  console.log(firstName);
-  console.log(lastName);
-  console.log(email);
+  const { firstName, lastName, email } = req.body;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  // Relevant MailChimp Documentation
+  // https://mailchimp.com/developer/marketing/docs/fundamentals/
+  // https://mailchimp.com/developer/marketing/api/lists/
+
+  const dataCenter = process.env.MAILCHIMP_DC;
+  const audienceId = process.env.MAILCHIMP_AUDIENCE_ID;
+  const url = `https://${dataCenter}.api.mailchimp.com/3.0/lists/${audienceId}`;
+
+  const options = {
+    method: 'POST',
+    auth: `key:${process.env.MAILCHIMP_KEY}`,
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on('data', (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, () => {
