4d15f9bbaad22128546d6ced334621256b3c96ce
E. Hannah <echannah631@gmail.com>
2023-03-02 12:05:57 -0600

    getting static files on site, set up post request

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -2,7 +2,21 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
-app.use(express.urlencoded()); // replaces body-parser
+app.use(express.urlencoded({ extended: true })); // replaces body-parser
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/signup.html`);
+});
+
+app.post('/', (req, res) => {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  console.log(firstName);
+  console.log(lastName);
+  console.log(email);
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
