825c739817fd7127fd31ba4dbb7fae2138ecf05e
E. Hannah <echannah631@gmail.com>
2023-03-02 10:46:26 -0600

    using express' built in body-parser

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+const express = require('express');
+const request = require('request');
 
-const app = express()
-
+const app = express();
+app.use(express.urlencoded()); // replaces body-parser
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-})
+  console.log('Server is running on port 3000');
+});
